820b430f915d4bbcf2ca3ef5a3f03aab6615828e
divyanshu1234 <divyanshu.pundir@gmail.com>
2020-04-07 01:12:56 +0530

    fix: Top message sender bug

--- chatalyzer/analysis.py
+++ chatalyzer/analysis.py
@@ -7,6 +7,7 @@ KEY_TIME = 'Time'
 KEY_AUTHOR = 'Author'
 KEY_MESSAGE = 'Message'
 KEY_MESSAGE_COUNT = 'Message Count'
+KEY_DATE_TIME = 'Date Time'
 
 TAG_MEDIA_OMITTED = '<Media omitted>'
 
@@ -17,16 +18,20 @@ def am_pm_to_24hr(df):
     return df2
 
 
-def drop_none_author(df):
+def drop_none_author(df, reset_index=True):
     df2 = df.dropna()
+    if reset_index:
+        df2.reset_index(drop=True, inplace=True)
     return df2
 
 
-def drop_media_messages(df):
-    media_messages = get_media_messages(df)
-    media_indices = media_messages.index
-    df2 = df.drop(index=media_indices)
-    return df2
+def drop_media_messages(df, reset_index=True):
+    media_message_df = get_media_messages(df)
+    media_indices = media_message_df.index
+    media_omitted_df = df.drop(index=media_indices)
+    if reset_index:
+        media_omitted_df.reset_index(drop=True, inplace=True)
+    return media_omitted_df
 
 
 def get_media_messages(df):
@@ -34,36 +39,34 @@ def get_media_messages(df):
     return media_messages
 
 
-def add_hour_minute_second(df):
-    pass
-
-
-def add_letter_count(df):
-    pass
-
-
-def add_word_count(df):
-    pass
+def get_date_time(df):
+    df2 = am_pm_to_24hr(df)
+    date_time_df = pd.to_datetime(df2[KEY_DATE]+':'+df2[KEY_TIME], format='%d/%m/%y:%H:%M:%S')
+    return date_time_df
 
 
 def get_top_message_senders(df, n_authors=10):
-    df2 = df.goupby(KEY_AUTHOR, sort=True).count()
-    df3 = pd.DataFrame(df2[[KEY_AUTHOR, KEY_MESSAGE]].values, columns=[KEY_AUTHOR, KEY_MESSAGE_COUNT])
-    return df3.loc[:(n_authors-1)]
+    df2 = df.groupby(KEY_AUTHOR, as_index=False).count()\
+        .sort_values(by=[KEY_MESSAGE], ascending=False)\
+        .reset_index(drop=True)\
+        .rename(columns={KEY_MESSAGE: KEY_MESSAGE_COUNT})
+
+    top_message_df = df2[[KEY_AUTHOR, KEY_MESSAGE_COUNT]]
+    return top_message_df.head(n_authors)
 
 
 def get_top_media_senders(df, n_authors=10):
     media_messages = get_media_messages(df)
-    df2 = media_messages.groupby(KEY_AUTHOR, sort=True).count()
-    df3 = pd.DataFrame(df2[[KEY_AUTHOR, KEY_MESSAGE]].values, columns=[KEY_AUTHOR, KEY_MESSAGE_COUNT])
-    return df3.loc[:(n_authors-1)]
+    df2 = media_messages.groupby(KEY_AUTHOR, as_index=False).count()\
+        .sort_values(by=[KEY_MESSAGE], ascending=False)\
+        .reset_index(drop=True)\
+        .rename(columns={KEY_MESSAGE: KEY_MESSAGE_COUNT})
+
+    top_media_df = df2[[KEY_AUTHOR, KEY_MESSAGE_COUNT]]
+    return top_media_df.head(n_authors)
 
 
-def get_busy_dates(df, n_days=10):
-    pass
-
-
-def get_busy_time_of_day(df, n_points=10):
+def get_busy_dates(df, n_dates=10):
     pass
 
 
@@ -71,6 +74,20 @@ def get_busy_hours(df, n_hours=10):
     pass
 
 
+def add_word_count(df):
+    pass
+
+
+def add_letter_count(df):
+    pass
+
+
+def add_date_time(df):
+    df2 = df.copy()
+    date_time_df = get_date_time(df)
+    df2[KEY_DATE_TIME] = date_time_df
+    return df2
+
+
 def debug_df(df):
-    df = am_pm_to_24hr(df)
     ipdb.set_trace()
